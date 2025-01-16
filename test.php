1084fdf810d26a18edbaa4e1045bd2dc9e01ad49
Mohammed Ali Boutaine <111361086+mohammed-ali-boutaine@users.noreply.github.com>
2025-01-16 15:12:18 +0100

    test register and login

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "./classes/Student.php";
+require_once "./classes/Teacher.php";
+require_once "./classes/User.php";
+
+
+// $test = Student::register("ali","test@gmail.com","test","test.png");
+// $test = Teacher::register("teacher1","teacher@gmail.com","teacher","teacher.png");
+
+// $test = User::login("test@gmail.com","test");
+// $test = User::login("teacher@gmail.com","teacher");
+
+
+
+// echo "<pre>";
+// print_r($test);
+// echo "</pre>";
