f53d7cec9f6abfd2e07fc47853e203e9887476e5
Mohammed Ali Boutaine <111361086+mohammed-ali-boutaine@users.noreply.github.com>
2025-01-16 16:44:41 +0100

    create helper function and add auth middle wares

--- functions/helper.php
+++ functions/helper.php
@@ -1,10 +1,11 @@
 <?php
 
+require_once __DIR__ . "/../classes/Auth.php";
 
 // Helper function to sanitize input
-function sanitize_input($data) 
+function sanitize_input($data)
 {
-    return htmlspecialchars(stripslashes(trim($data)));
+    return htmlspecialchars(stripslashes(trim($data)), ENT_QUOTES, 'UTF-8');
 }
 
 // Helper function to redirect
@@ -17,19 +18,55 @@ function validate_email($email)
 {
     return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
-
-function check_user_authentication()
+function tagHandler($content)
 {
 
-    // authentification login here
+    // content example
+    // $string = "hello this is a post about #coding and #programming";
+    $tags = [];
+    $words = explode(" ", $content);
+    for ($i = 0; $i < sizeof($words); $i++) {
+        $word = $words[$i];
 
+        if ($words[$i][0] === "#" && strlen($word) > 1) {
+
+            $tag = substr(strtolower($words[$i]), 1);
+
+            if (!in_array($tag, $tags)) {
+                $tags[] = $tag;
+            }
+            $words[$i] = "<a href='page#$tag'>" . $words[$i] . "</a>";
+        }
+    }
+
+    $content = implode(" ", $words);
+
+    $result = ["tags" => $tags, "content" => $content];
+
+    return $result;
 }
 
-function requireAuth()
+
+function requireRole($role)
 {
-    if (!check_user_authentication()) {
-        header("Location: login.php");
-        exit();
+    if (!Auth::isAuth()) {
+        redirect("/pages/login.php");
+    }
+
+    $hasRole = false;
+    switch ($role) {
+        case 'admin':
+            $hasRole = Auth::isAdmin();
+            break;
+        case 'teacher':
+            $hasRole = Auth::isTeacher();
+            break;
+        case 'student':
+            $hasRole = Auth::isStudent();
+            break;
+    }
+
+    if (!$hasRole) {
+        redirect("/pages/login.php");
     }
 }
-
